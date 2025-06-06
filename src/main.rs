ff4b28d9675348c2275742830421e787d7af1e8d
Jasper De Moor <jasperdemoor@gmail.com>
2025-06-06 14:23:50 +0200

    health endpoints

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, routing::get};
+
+mod routes;
+
+const PORT: u16 = 1234;
+
+#[tokio::main]
+async fn main() {
+    // build our application with a single route
+    let app = Router::new()
+        .route("/", get(|| async { "Hello, World!" }))
+        .route("/health", get(routes::health::handler))
+        .route("/ready", get(routes::ready::handler));
+
+    let addr: String = format!("0.0.0.0:{}", PORT);
+    let listener = tokio::net::TcpListener::bind(&addr).await.unwrap();
+    println!("Starting server on {}", &addr);
+    axum::serve(listener, app).await.unwrap();
 }
