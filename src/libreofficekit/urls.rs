e6eb76d3dfc353302f25d3c7aceb05a46756ccf1
Jasper De Moor <jasperdemoor@gmail.com>
2025-06-06 18:38:08 +0200

    cleanup

--- src/libreofficekit/urls.rs
+++ src/libreofficekit/urls.rs
@@ -15,16 +15,6 @@ impl DocUrl {
         self.0.as_ptr()
     }
 
-    /// Converts a local relative path into an absolute path that LibreOffice can use
-    pub fn from_relative_path<S: AsRef<str>>(path: S) -> Result<DocUrl, OfficeError> {
-        let path: &str = path.as_ref();
-        let abs_path = std::fs::canonicalize(path).map_err(|err| {
-            OfficeError::OfficeError(format!("Does the file exist at {}? {}", path, err))
-        })?;
-
-        Self::from_absolute_path(abs_path.display().to_string())
-    }
-
     /// Converts a local absolute path into a [DocUrl] the path MUST be an absolute path
     /// otherwise you'll get an error from LibreOffice
     ///
@@ -58,80 +48,10 @@ impl DocUrl {
 
         Self::from_absolute_path(abs_path.display().to_string())
     }
-
-    /// Converts a remote URI path into a [DocUrl]
-    pub fn from_remote_uri<S: AsRef<str>>(uri: S) -> Result<DocUrl, OfficeError> {
-        let uri: &str = uri.as_ref();
-
-        // Ensure the URL is valid
-        _ = Url::parse(uri)
-            .map_err(|_| OfficeError::OfficeError(format!("failed to parse uri {}", uri)))?;
-
-        let value_str = CString::new(uri)?;
-
-        Ok(DocUrl(value_str))
-    }
 }
 
 impl fmt::Display for DocUrl {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "{}", self.0.to_string_lossy())
-    }
-}
-
-#[cfg(test)]
-mod test {
-    use super::DocUrl;
-
-    /// Tests a valid relative path
-    #[test]
-    fn test_relative() {
-        let path = "./src";
-        let _url = DocUrl::from_relative_path(path).unwrap();
-    }
-
-    /// Tests an invalid relative path
-    #[test]
-    fn test_invalid_relative() {
-        let path = "/__ABSOLUTE_PATH_THAT_DOES_NOT_EXIST__";
-        let _url = DocUrl::from_relative_path(path).unwrap_err();
-    }
-
-    /// Tests a valid absolute URL
-    #[test]
-    fn test_absolute() {
-        #[cfg(target_os = "windows")]
-        let path = "C://";
-        #[cfg(not(target_os = "windows"))]
-        let path = "/src";
-
-        let _url = DocUrl::from_absolute_path(path).unwrap();
-    }
-
-    /// Tests a non absolute URL fails the checks
-    #[test]
-    fn test_invalid_absolute() {
-        let path = "./src";
-        let _url = DocUrl::from_absolute_path(path).unwrap_err();
-    }
-
-    /// Tests a remote URL path
-    #[test]
-    fn test_remote_path() {
-        let path = "http://localhost:5555/file.docx";
-        let _url = DocUrl::from_remote_uri(path).unwrap();
-
-        let path = "file://file.docx";
-        let _url = DocUrl::from_remote_uri(path).unwrap();
-    }
-
-    /// Tests an invalid remote paths
-    #[test]
-    fn test_invalid_remote_path() {
-        let path = "h/malformed";
-        let _url = DocUrl::from_remote_uri(path).unwrap_err();
-
-        let path = "h";
-        let _url = DocUrl::from_remote_uri(path).unwrap_err();
     }
 }
